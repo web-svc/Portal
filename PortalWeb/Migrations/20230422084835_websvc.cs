6972fd534835ec12454360c489f0be5aef777647
Narender Kumar <Narender.Kumar@multichoice.co.za>
2023-04-24 16:50:07 +0530

    Add project files.

---
+++ PortalWeb/Migrations/20230422084835_websvc.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortalWeb.Migrations
+{
+    public partial class websvc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
