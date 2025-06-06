95d8cc885bc2cf292a7cb5bd0d58898250d6cc6e
Narender Kumar <Narender.Kumar@multichoice.co.za>
2025-06-06 23:18:56 +0530

    update

--- PortalWeb/Data/ApplicationDbContext.cs
+++ PortalWeb/Data/ApplicationDbContext.cs
@@ -36,7 +36,7 @@ namespace PortalWeb.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            => optionsBuilder.UseSqlServer("Server=.;Database=dbLoginService;Trusted_Connection=True; TrustServerCertificate=True;");
+            => optionsBuilder.UseSqlServer("Data Source=SQL5110.site4now.net;Initial Catalog=db_a98287_dbloginservice;User Id=db_a98287_dbloginservice_admin;Password=test@123");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
